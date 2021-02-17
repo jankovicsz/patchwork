501dff7c1834835b233547d6457b2ed1e1fda410
Jankovics Zoltan <jankovicsz@gmail.com>
2021-02-17 22:56:06 +0100

    Login app has been add

--- test/firebase2/frontend/public/app.js
+++ test/firebase2/frontend/public/app.js
@@ -5,8 +5,26 @@ const switchToLoginLink = document.querySelector('.registrated-user a');
 switchToLoginLink.addEventListener('click', () => toggleLoginAndReg());
 
 function toggleLoginAndReg() {
-  const loginForm = document.getElementById('login-container');
-  const regForm = document.getElementById('reg-container');
-  loginForm.classList.toggle('d-none');
-  regForm.classList.toggle('d-none');
+  const loginFormDiv = document.getElementById('login-container');
+  const regFormDiv = document.getElementById('reg-container');
+  loginFormDiv.classList.toggle('d-none');
+  regFormDiv.classList.toggle('d-none');
+}
+
+function toggleLoggedIn() {
+  const loginFormDiv = document.getElementById('login-container');
+  const regFormDiv = document.getElementById('reg-container');
+  const loggedinDiv = document.getElementById('loggedin-container');
+  loggedinDiv.classList.remove('d-none');
+  loginFormDiv.classList.add('d-none');
+  regFormDiv.classList.add('d-none');
+}
+
+function toggleLogOut() {
+  const loginFormDiv = document.getElementById('login-container');
+  const regFormDiv = document.getElementById('reg-container');
+  const loggedinDiv = document.getElementById('loggedin-container');
+  loggedinDiv.classList.add('d-none');
+  loginFormDiv.classList.remove('d-none');
+  regFormDiv.classList.add('d-none');
 }
--- test/firebase2/frontend/public/firebase.js
+++ test/firebase2/frontend/public/firebase.js
@@ -9,42 +9,82 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-/* 
-firebase
+// const auth = firebase.auth();
+
+const regForm = document.getElementById('reg-form');
+regForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = regForm['reg-email'].value;
+  const password = regForm['reg-password'].value;
+  
+  firebase
   .auth()
   .createUserWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
+    regForm.reset();
+    toggleLoginAndReg();
     // ...
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
     // ..
   });
+});
 
-firebase
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+  
+  firebase
   .auth()
   .signInWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
+    loginForm.reset();
+    toggleLoggedIn();
     // ...
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // ..
   });
- */
+});
+
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
+    // const uid = user.uid;
+    console.log(user);
+    const email = document.querySelector('#greeting span');
+    email.textContent = user.email;
     // ...
   } else {
     // User is signed out
     // ...
   }
 });
+
+const logOut = document.getElementById('logout-button');
+logOut.addEventListener('click', (e) => {
+  e.preventDefault();
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    toggleLogOut();
+  }).catch((error) => {
+    // An error happened.
+  });
+});
+
+/* firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+}); */
